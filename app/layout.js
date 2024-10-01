953ca6f4fe89725847bdb9d72847e0b58d5c47b0
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-01 15:08:25 +0300

    layouta container divi eklendi, cssi ayarlandı

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="tr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div class="container">{children}</div>
       </body>
     </html>
   );
