671a716aa6366769eab72445a2b305248173b255
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-02 03:21:52 +0300

    hero sayfası tamamlandı / css tamamlandı

--- app/hero/page.jsx
+++ app/hero/page.jsx
@@ -9,11 +9,11 @@ export default function Hero() {
 
       <div className="hero-content-area">
         <h3>Merhaba! <br />
-          Ben Sudenur,</h3>
+          Ben  <span className="sudenur"> Sudenur</span>,</h3>
         <div className="hero-content">
           <p>Merhaba! Ben Sudenur Taştekin, 22 yaşındayım. İstanbul Üniversitesi-Cerrahpaşa'da İşletme Yönetimi bölümünden mezun oldum ve şu anda <a target="_blank" href="https://acunmedyaakademi.com/"><strong>Acunmedya Akademi</strong></a>'de Front-End Yazılım Geliştirme eğitimi alıyorum. Yenilikçi çözümler üretmekten ve kod yazmaktan büyük keyif alıyorum. Tasarımın gücünü teknolojiyle birleştirerek kullanıcı deneyimini en üst seviyeye çıkarmak benim için bir tutku haline geldi. Frontend geliştirme konusundaki bilgi ve becerilerimi sürekli olarak geliştiriyor ve sektördeki en son trendleri takip ediyorum. Yenilikçi fikirlerimi ve enerjimi bir projeye dahil etmek isterseniz, sizinle işbirliği yapmaktan mutluluk duyarım. Beraber çalışmak için sabırsızlanıyorum!</p>
         </div>
-        <button><a class="contactBtn" href="mailto:sude348tastekin@gmail.com">Bana Ulaşın</a></button>
+        <a className="contactBtn" href="mailto:sude348tastekin@gmail.com">Bana Ulaşın</a>
       </div>
     </div>
   )
