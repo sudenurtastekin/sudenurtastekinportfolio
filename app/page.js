c619686b7bbe0366e7cc7a80588ab35161371527
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-02 03:03:57 +0300

    hero oluşturuldu / cssi ayarlanıyor

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Nav from  "@/app/nav/page"
+import Hero from  "@/app/hero/page"
+
 
 export default function Home() {
   return (
     <>
+    <Hero/>
+
     </>
   );
 }
